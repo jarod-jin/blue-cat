8197bcdcc9944ea48cd11c3acb5859215aae1f1c
Jarod Jin <kira277@163.com>
2019-12-26 18:27:11 +0800

    新建文件对象

--- blue-cat-general/src/main/java/cn/jarod/bluecat/general/entity/ReleaseNoteDO.java
+++ blue-cat-general/src/main/java/cn/jarod/bluecat/general/entity/ReleaseNoteDO.java
@@ -4,6 +4,7 @@ import cn.jarod.bluecat.core.entity.BaseEntity;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 import org.hibernate.annotations.Type;
@@ -21,30 +22,31 @@ import java.util.List;
 @Entity
 @Getter
 @Setter
+@ToString
 @DynamicInsert
 @DynamicUpdate
 @EqualsAndHashCode(callSuper=true)
 @Table(name = "release_note", indexes = {@Index(columnList ="terminalType,releaseVersion", name="ReleaseVersionIndex")})
 public class ReleaseNoteDO extends BaseEntity {
 
-    //版本号
+    /**版本号*/
     @Column(nullable = false, columnDefinition=("varchar(50) comment '版本号'"))
     private String releaseVersion;
 
-    //字典键-值
+    /**字典键-值*/
     @Type(type = "json")
     @Column(nullable = false, columnDefinition=("json comment '发布内容'"))
     private List<String> releaseNote;
 
-    //版本序号
+    /**版本序号*/
     @Column(nullable = false, columnDefinition=("decimal(10,1) default 99 comment '版本序号'"))
     private BigDecimal buildNo;
 
-    //终端类型
+    /**终端类型*/
     @Column(nullable = false, columnDefinition=("varchar(20) default 'pc' comment '终端类型'"))
     private String terminalType;
 
-    //关联系统编号
+    /**关联系统编号*/
     @Column(nullable = false, columnDefinition=("varchar(30) default 'root' comment '关联系统编号'"))
     private String sysCode;
 
